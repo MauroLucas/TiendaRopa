85ca41911162da53d483c13ec3f8a836e4eb4b7e
Mauro Lucas <30880379+MauroLucas@users.noreply.github.com>
2021-07-31 10:28:23 -0300

    se crearon las clases Cotizacion y Vendedor

---
+++ TiendaRopa/TiendaRopa/modelo/Cotizacion.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TiendaRopa.modelo
+{
+    class Cotizacion
+    {
+        private int id;
+        private DateTime fechaHora;
+        private String codVendedor;
+        private String prenda;
+        private int cantUnidades;
+        private float precioFinal;
+
+        public Cotizacion() { }
+        public Cotizacion(int id, DateTime fechaHora, string codVendedor, string prenda, int cantUnidades)
+        {
+            this.id = id;
+            this.fechaHora = fechaHora;
+            this.codVendedor = codVendedor;
+            this.prenda = prenda;
+            this.cantUnidades = cantUnidades;            
+        }
+
+        public int Id { get => id; set => id = value; }
+        public DateTime FechaHora { get => fechaHora; set => fechaHora = value; }
+        public string CodVendedor { get => codVendedor; set => codVendedor = value; }
+        public string Prenda { get => prenda; set => prenda = value; }
+        public int CantUnidades { get => cantUnidades; set => cantUnidades = value; }
+        public float PrecioFinal { get => precioFinal;}
+    }
+}
---
+++ TiendaRopa/TiendaRopa/modelo/Vendedor.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TiendaRopa.modelo
+{
+    class Vendedor
+    {
+        private String nombre;
+        private String apellido;
+        private String codVendedor;
+        private List<Cotizacion> lstCotizaciones;
+
+        public Vendedor() { }
+        public Vendedor(string nombre, string apellido, string codVendedor, List<Cotizacion> lstCotizaciones)
+        {
+            this.nombre = nombre;
+            this.apellido = apellido;
+            this.codVendedor = codVendedor;
+            this.lstCotizaciones = lstCotizaciones;
+        }
+
+        public string Nombre { get => nombre; set => nombre = value; }
+        public string Apellido { get => apellido; set => apellido = value; }
+        public string CodVendedor { get => codVendedor; set => codVendedor = value; }
+        internal List<Cotizacion> LstCotizaciones { get => lstCotizaciones; set => lstCotizaciones = value; }
+    }
+}
