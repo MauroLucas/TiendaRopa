8848c3bdd7ecbda1d4fa9685f594073fa1167889
Mauro Lucas <30880379+MauroLucas@users.noreply.github.com>
2021-07-31 10:45:10 -0300

    se crearon las clases Prenda y Tienda

---
+++ TiendaRopa/TiendaRopa/modelo/Prenda.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TiendaRopa.modelo
+{
+    class Prenda
+    {
+        private String calidad;
+        private float precio;
+        private int cantStock;
+
+        public Prenda() { }
+        public Prenda(string calidad, float precio, int cantStock)
+        {
+            this.calidad = calidad;
+            this.precio = precio;
+            this.cantStock = cantStock;
+        }
+
+        public string Calidad { get => calidad; set => calidad = value; }
+        public float Precio { get => precio; set => precio = value; }
+        public int CantStock { get => cantStock; set => cantStock = value; }
+    }
+}
---
+++ TiendaRopa/TiendaRopa/modelo/Tienda.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TiendaRopa.modelo
+{
+    class Tienda
+    {
+        private String nombre;
+        private String direccion;
+        private List<Prenda> lstPrendas;
+
+        public Tienda() { }
+        public Tienda(string nombre, string direccion, List<Prenda> lstPrendas)
+        {
+            this.nombre = nombre;
+            this.direccion = direccion;
+            this.lstPrendas = lstPrendas;
+        }
+
+        public string Nombre { get => nombre; set => nombre = value; }
+        public string Direccion { get => direccion; set => direccion = value; }
+        internal List<Prenda> LstPrendas { get => lstPrendas; set => lstPrendas = value; }
+    }
+}
